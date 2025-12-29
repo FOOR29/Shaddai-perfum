d53bf40bd1b89e24dc90c52ec0df717d9cb20f45
Forlan Ordoñez <nalrofor@gmail.com>
2025-12-29 16:49:09 -0500

    feat: todo listo para produccion

--- src/components/ui/PerfumCard.tsx
+++ src/components/ui/PerfumCard.tsx
@@ -56,7 +56,7 @@ const PerfumeCard = ({
                     <h3 className="font-extrabold text-lg text-titular line-clamp-1">
                         {name}
                     </h3>
-                    <span className="text-sm text-center bg-chip-inactivo py-0.5 px-3 rounded-full text-subtitulo font-extrabold whitespace">
+                    <span className="text-sm text-center bg-chip-inactivo py-0.5 px-3 rounded-full text-secundario font-extrabold whitespace">
                         {getGenderLabel(gender)}
                     </span>
                 </div>
